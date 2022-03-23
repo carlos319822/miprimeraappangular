68f80886195176ac960fd0e471f4348e22903def
carlos319822 <72896010+carlos319822@users.noreply.github.com>
2022-03-23 15:55:02 -0500

    mi primera version de mi proyecto angular con super poblacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-primera-app';
+  titulo = 'Mi Primer App en Angular';
+  
 }
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    styleUrls:['./personas.component.css']
+    
+
+})
+export class PersonasComponent{
+    deshabilitar = false;
+    
+    mensaje = '';
+    titulo = 'Ingeniero';
+    mostrar=false;
+
+
+    agregarPersona(){
+        this.mostrar = true;
+        this.mensaje = 'Persona agregada';
+    }
+
+    /*modificarTitulo(event: Event){
+        console.log('Entrando a metodo modificar titulo');
+        this.titulo = (<HTMLInputElement>event.target).value;
+    }*/
+}
